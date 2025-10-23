76fcf1f1f61aae44b16f4b81bcea18ce164c8c02
hilmy <hilmyahmadnaufal@gmail.com>
2025-10-23 22:41:27 +0700

    feat: add basic navigation on template

---
+++ apps/template/src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      component: () => import("@/views/Home.vue"),
+      children: [
+        {
+          path: '',
+          name: 'home',
+        }
+      ]
+    }
+  ]
+})
+
+export default router
---
+++ apps/template/src/views/Home.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container">
+        <nav class="nav-wrapper">
+            <ul class="nav">
+                <li>
+                    <RouterLink :to="{ name: 'home' }">
+                        Home
+                    </RouterLink>
+                </li>
+            </ul>
+        </nav>
+        <div class="wrapper">
+            <RouterView></RouterView>
+        </div>
+    </div>
+</template>
+
+<style></style>
